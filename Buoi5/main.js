b3c49c8b539e6ca1a5b21a3127ee1e86bac462a0
tiSt <titisme30012007@gmail.com>
2021-10-14 15:41:40 +0700

    Upload Buoi1Calculator

---
+++ Buoi5/main.js
@@ -0,0 +1,9 @@
+let key = "todolist";
+let todo = document.getElementById("todo");
+let add = document,getElementById("add");
+let clear = document,getElementById("clear");
+let result = document,getElementById("result");
+
+add.addEventListener("click, function"(){
+    let
+}
